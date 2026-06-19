3fc0851970c584cd515e0664ade313535f022a57
Eric Harvey <Eric.Harvey@ibm.com>
2026-06-19 12:16:00 -0400

    fixes remaining how_to and tutorial links

--- qiskit_addon_obp/utils/truncating.py
+++ qiskit_addon_obp/utils/truncating.py
@@ -26,7 +26,7 @@ from qiskit.quantum_info import SparsePauliOp
 class TruncationErrorBudget:
     """A class for storing the constants that determine the truncation error budget.
 
-    Refer to the `how-to guide <https://qiskit.github.io/qiskit-addon-obp/how_tos/truncate_operator_terms.html>`__ for a detailed discussion
+    Refer to the `how-to guide <https://qiskit.github.io/qiskit-addon-obp/guides/truncate_operator_terms.html>`__ for a detailed discussion
     on truncating operator terms during backpropagation and bounding the incurred error.
     """
 
@@ -44,7 +44,7 @@ class TruncationErrorBudget:
     """
     Indicates which Lp-norm is used for calculating truncation errors.
 
-    Refer to the `how-to guide <https://qiskit.github.io/qiskit-addon-obp/how_tos/bound_error_using_p_norm.html>`__ for a detailed
+    Refer to the `how-to guide <https://qiskit.github.io/qiskit-addon-obp/guides/bound_error_using_p_norm.html>`__ for a detailed
     conversation on bounding truncation error using higher Lp-norms.
     """
 
@@ -70,7 +70,7 @@ def setup_budget(
     This method makes the construction of a :class:`.TruncationErrorBudget` easier for an end-user.
     This error budget can be provided to the :meth:`~qiskit_addon_obp.backpropagate` method to
     enable the truncation of low-weight Pauli terms. Refer to the `how-to guide
-    <https://qiskit.github.io/qiskit-addon-obp/how_tos/truncate_operator_terms.html>`__ for a detailed discussion on truncating terms from
+    <https://qiskit.github.io/qiskit-addon-obp/guides/truncate_operator_terms.html>`__ for a detailed discussion on truncating terms from
     the output operator and bounding the incurred error.
 
     The construction logic is as follows:
@@ -101,7 +101,7 @@ def setup_budget(
         num_slices: The number of slices over which to distribute the budget. See above for more details.
         p_norm: The Lp norm of the error. This affects the gradual distribution of
             ``max_error_total`` in the case of ``num_slices`` also being set (see above). Refer to the
-            `how-to guide <https://qiskit.github.io/qiskit-addon-obp/how_tos/bound_error_using_p_norm.html>`__ for a detailed conversation
+            `how-to guide <https://qiskit.github.io/qiskit-addon-obp/guides/bound_error_using_p_norm.html>`__ for a detailed conversation
             on bounding truncation error using higher Lp-norms.
 
     Returns:
@@ -111,6 +111,7 @@ def setup_budget(
         ValueError: if ``max_error_per_slice`` and ``max_error_total`` are both ``None``.
 
     """
+
     if max_error_per_slice is None and max_error_total is None:
         raise ValueError("max_error_per_slice and max_error_total may not both be None")
 
